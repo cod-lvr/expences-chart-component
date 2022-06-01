937eb529039f27a2a3b45c373f95a7703efb3756
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-06-02 00:33:49 +0200

    finished component -responsive design

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-// import data from "./data/data.json";
+// import data from "./data/data.js";
 
 const ctx = document.getElementById("myChart");
 
@@ -8,18 +8,53 @@ const myChart = new Chart(ctx, {
     labels: ["mon", "tue", "wed", "thu", "fri", "sat", "sun"],
     datasets: [
       {
-        label: "reguler",
-        data: [17.45, 34.91, 3, 5, 2, 3],
+        label: "Max-Expence",
+        data: [
+          {
+            day: "mon",
+            amount: 17.45,
+          },
+          {
+            day: "tue",
+            amount: 34.91,
+          },
+          {
+            day: "wed",
+            amount: 52.36,
+          },
+          {
+            day: "thu",
+            amount: 31.07,
+          },
+          {
+            day: "fri",
+            amount: 23.39,
+          },
+          {
+            day: "sat",
+            amount: 43.28,
+          },
+          {
+            day: "sun",
+            amount: 25.48,
+          },
+        ],
         backgroundColor: [
           "hsl(10, 79%, 65%)",
           "hsl(10, 79%, 65%)",
-          "hsl(10, 79%, 65%)",
+          "hsl(186, 34%, 60%)",
           "hsl(10, 79%, 65%)",
           "rhsl(10, 79%, 65%)",
           "hsl(10, 79%, 65%)",
         ],
+        borderRadius: "12px",
       },
     ],
   },
-  options: {},
+  options: {
+    parsing: {
+      xAxisKey: "day",
+      yAxisKey: "amount",
+    },
+  },
 });
